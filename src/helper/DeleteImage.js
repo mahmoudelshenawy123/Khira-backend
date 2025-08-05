039ca4c591b1b19d1b3666ef5e80880af3e35001
mahmoudelshenawy123 <mahmoud_elshenawy6@outlook.com>
2025-08-06 02:08:48 +0300

    changes

--- src/helper/DeleteImage.js
+++ src/helper/DeleteImage.js
@@ -1,9 +1,9 @@
-const fs =require('fs')
-exports.DeleteImage=async (image)=>{
-    await new Promise((resolve,reject)=>{
-        fs.unlink(`public/uploads/${image}`,(err)=> {
-            console.log(err)
-            resolve()
-        })
+const fs = require('fs')
+exports.DeleteImage = async (image) => {
+  await new Promise((resolve, reject) => {
+    fs.unlink(`api/public/uploads/${image}`, (err) => {
+      console.log(err)
+      resolve()
     })
+  })
 }
--- src/helper/HelperFunctions.js
+++ src/helper/HelperFunctions.js
@@ -1,16 +1,16 @@
 function MergeImageLink(req, imageLink) {
-  const baseUrl = `${req.protocol}://${req.get('host')}/public/uploads/`;
-  return `${baseUrl}${imageLink}`;
+  const baseUrl = `${req.protocol}://${req.get('host')}/api/public/uploads/`
+  return `${baseUrl}${imageLink}`
 }
 
 function MergePDfLink(req, pdfLink) {
-  const baseUrl = `${req.protocol}://${req.get('host')}/public/files/`;
-  return `${baseUrl}${pdfLink}`;
+  const baseUrl = `${req.protocol}://${req.get('host')}/api/public/files/`
+  return `${baseUrl}${pdfLink}`
 }
 function SplitImageLink(req, imageLink) {
-  const baseUrl = `${req.protocol}://${req.get('host')}/public/uploads/`;
+  const baseUrl = `${req.protocol}://${req.get('host')}/api/public/uploads/`
 
-  return imageLink.split(baseUrl)[1];
+  return imageLink.split(baseUrl)[1]
 }
 function PaginateSchema(currentPage, pages, count, data) {
   return {
@@ -18,15 +18,19 @@ function PaginateSchema(currentPage, pages, count, data) {
     pages,
     count,
     data,
-  };
+  }
 }
 function ResponseSchema(message, status, data) {
   return {
     message,
     status,
     data,
-  };
+  }
 }
 module.exports = {
-  MergeImageLink, SplitImageLink, ResponseSchema, PaginateSchema,MergePDfLink
-};
+  MergeImageLink,
+  SplitImageLink,
+  ResponseSchema,
+  PaginateSchema,
+  MergePDfLink,
+}
